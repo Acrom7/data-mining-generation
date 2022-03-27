90604b737480f2175333a521a9c48c50fbdd8ea4
Dmitriy Zvezdetskiy <zvezdetskiy@taptima.ru>
2022-03-27 20:32:03 +1000

    add export button for inductive knowledge base

--- src/presentation/component/page/inductiveKnowledgeBase/Table/index.tsx
+++ src/presentation/component/page/inductiveKnowledgeBase/Table/index.tsx
@@ -1,27 +1,48 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Table as AntdTable } from 'antd';
-import { recoveryDataKnowledgeBase } from './inductiveRecovery';
+import { Button, Space, Table as AntdTable } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+import exportFromJSON, { ExportType } from 'export-from-json';
 import intervalToString from 'helper/interval/intervalToString';
+import { HeaderWrapper } from 'presentation/component/page/modelKnowledgeBase/Table/styles';
+import { recoveryDataKnowledgeBase } from './inductiveRecovery';
 
 type PropsT = {
     values: ThirdValueT[];
 };
 
-
 const Table: FC<PropsT> = (props) => {
     const { values } = props;
     const [tableData, setTableData] = useState<ValueT[]>([]);
-    const [isGenerating, setIsGenerating] = useState(false);
+
+    const exportFile = async (type: ExportType) => {
+        exportFromJSON({
+            data: tableData.map(({ ...rest }) => ({ ...rest })),
+            fileName: 'InductiveKnowledgeBase',
+            exportType: type,
+        });
+    };
 
     useEffect(() => {
-        const res = recoveryDataKnowledgeBase(values)
+        const res = recoveryDataKnowledgeBase(values);
 
-        setTableData(res)
+        setTableData(res);
     }, []);
 
     return (
         <AntdTable
-            loading={isGenerating}
+            title={() => (
+                <HeaderWrapper>
+                    <Space>
+                        <Button
+                            icon={<DownloadOutlined />}
+                            onClick={() => exportFile('json')}
+                            type="primary"
+                        >
+                            Скачать JSON
+                        </Button>
+                    </Space>
+                </HeaderWrapper>
+            )}
             dataSource={tableData}
             columns={[
                 {
